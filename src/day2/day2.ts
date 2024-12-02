ff011d7448cfc9d4625c90fc520305daa4d2b980
Genie <genie@redairship.com>
2024-12-02 21:39:05 +0700

    feat: add solution for day2 part 2

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -27,4 +27,39 @@ function getNumOfSafeReport(filePath: string): number {
     return numOfSafeReported;
 }
 
+function getNumOfDampenerSafeReport(filePath: string): number {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const lines = fileContent.split('\n');
+
+    let numOfSafeReported = 0;
+
+    lines.forEach(line => {
+        if (line == null || line.trim() == '') return;
+
+        const levels = line.trim().split(' ').map(Number);
+
+        for (let idx = 0; idx < levels.length; idx++) {
+
+            let testLevels = [...levels.slice(0, idx), ...levels.slice(idx + 1, levels.length)];
+
+            let subLevels = [];
+
+            for (let i = 0; i < testLevels.length - 1; i++) {
+                subLevels.push(testLevels[i + 1] - testLevels[i]);
+            }
+            let isSafe = subLevels.every(num => num <= 3 && num >= 1)
+                || subLevels.every(num => num >= -3 && num <= -1);
+
+            if (isSafe) {
+                numOfSafeReported++;
+                break;
+            }
+        }
+    });
+
+    return numOfSafeReported;
+}
+
+
 console.log(getNumOfSafeReport(filePath));
+console.log(getNumOfDampenerSafeReport(filePath));
