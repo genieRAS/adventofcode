24fe30884c038452160659e42d85b3b41253d430
Genie <genie@redairship.com>
2024-12-03 11:12:20 +0700

    refactor: refactor day2 solution

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -3,6 +3,18 @@ import * as fs from 'fs';
 // const filePath = 'input-sample.txt';
 const filePath = 'input.txt';
 
+function isSaveReport(levels: number[]): boolean {
+    let subLevels = [];
+
+    for (let i = 0; i < levels.length - 1; i++) {
+        subLevels.push(levels[i+1] - levels[i]);
+    }
+    let isSafe = subLevels.every(num => num <= 3 && num >= 1)
+        || subLevels.every(num => num >= -3 && num <= -1);
+
+    return isSafe
+}
+
 function getNumOfSafeReport(filePath: string): number {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const lines = fileContent.split('\n');
@@ -12,15 +24,7 @@ function getNumOfSafeReport(filePath: string): number {
         if (line == null || line.trim() == '') return;
 
         const levels = line.trim().split(' ').map(Number);
-
-        let subLevels = [];
-
-        for (let i = 0; i < levels.length - 1; i++) {
-            subLevels.push(levels[i+1] - levels[i]);
-        }
-        let isSafe = subLevels.every(num => num <= 3 && num >= 1)
-            || subLevels.every(num => num >= -3 && num <= -1);
-
+        let isSafe = isSaveReport(levels);
         numOfSafeReported += isSafe ? 1 : 0;
     });
 
@@ -39,17 +43,8 @@ function getNumOfDampenerSafeReport(filePath: string): number {
         const levels = line.trim().split(' ').map(Number);
 
         for (let idx = 0; idx < levels.length; idx++) {
-
             let testLevels = [...levels.slice(0, idx), ...levels.slice(idx + 1, levels.length)];
-
-            let subLevels = [];
-
-            for (let i = 0; i < testLevels.length - 1; i++) {
-                subLevels.push(testLevels[i + 1] - testLevels[i]);
-            }
-            let isSafe = subLevels.every(num => num <= 3 && num >= 1)
-                || subLevels.every(num => num >= -3 && num <= -1);
-
+            let isSafe = isSaveReport(testLevels);
             if (isSafe) {
                 numOfSafeReported++;
                 break;
